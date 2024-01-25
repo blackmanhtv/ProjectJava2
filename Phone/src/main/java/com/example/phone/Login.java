4aab84b6e26e16353d3518b4266491a276fb854b
ngockhanh <zaiphonuibg@gmail.com>
2024-01-25 19:39:38 +0700

    projectsmarphone

---
+++ Phone/src/main/java/com/example/phone/Login.java
@@ -0,0 +1,24 @@
+package com.example.phone;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Login extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(Login.class.getResource("login.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 500, 400);
+        stage.setTitle("Hello khánh!");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
+
