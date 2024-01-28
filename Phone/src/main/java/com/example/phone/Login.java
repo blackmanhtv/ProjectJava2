664e9f3a915fd70e123e7ba62e3994a1154c9e93
Tống Văn Mạnh <tvm19122002@gmail.com>
2024-01-28 16:17:46 +0700

    giao dien home page

--- Phone/src/main/java/com/example/phone/Login.java
+++ Phone/src/main/java/com/example/phone/Login.java
@@ -10,8 +10,8 @@ import java.io.IOException;
 public class Login extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(Login.class.getResource("login.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 500, 400);
+        FXMLLoader fxmlLoader = new FXMLLoader(Login.class.getResource("home.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 600, 400);
         stage.setTitle("Hello khánh!");
         stage.setScene(scene);
         stage.show();
